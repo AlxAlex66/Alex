def6c8e03f0dc0641678d7e9cacc5d9cc6ab669d
Alexandru  Constantin <alxalex66@yahoo.com>
2021-10-19 22:18:47 +0300

    Mici ajustari navbar

--- Script/Script.js
+++ Script/Script.js
@@ -1,10 +1,25 @@
 function navSlide(){
   const butonMeniu = document.querySelector('.buton-mobil');
   const navigatie = document.querySelector('.lista-navigare');
+  const paginiMeniu = document.querySelectorAll('.lista-navigare li')
 
     butonMeniu.addEventListener('click', function(){
-    navigatie.classList.toggle('lista-navigare-activare');
+      // iesire bara de navigare
+      navigatie.classList.toggle('lista-navigare-activare');
+
+    // Animare butoanele ce ne trimit la pagini
+    paginiMeniu.forEach( (link, index) => {
+      if(link.style.animation){
+        link.style.animation='';
+      }else {
+        link.style.animation=`miscareMeniu 0.5 ease forwards $(index /7 + 0.3)s
+      }
+
+    });
   });
+
+
+
 }
 
 navSlide();
