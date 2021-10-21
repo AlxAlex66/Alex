29ddcd95f0834ae0a7ffa4442449e14c769c3c2e
Alexandru Buhai <alxalex66@yahoo.com>
2021-10-21 16:03:46 +0300

    1

--- Script/Script.js
+++ Script/Script.js
@@ -1,4 +1,4 @@
-var siteWidth = 1280;
+var siteWidth = 500;
 var scale = screen.width /siteWidth;
 
 document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
