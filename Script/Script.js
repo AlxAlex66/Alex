a5e610fb6f4af07665072600d8924d798bfd9464
Alexandru Buhai <alxalex66@yahoo.com>
2021-10-21 14:50:44 +0300

    Terminare Bara de navigare

--- Script/Script.js
+++ Script/Script.js
@@ -1,22 +1,31 @@
 function navSlide(){
+  //elemente selectate
   const butonMeniu = document.querySelector('.buton-mobil');
   const navigatie = document.querySelector('.lista-navigare');
   const paginiMeniu = document.querySelectorAll('.lista-navigare li')
 
-    butonMeniu.addEventListener('click', function(){
+      butonMeniu.addEventListener('click', function() {
       // iesire bara de navigare
       navigatie.classList.toggle('lista-navigare-activare');
 
-    // Animare butoanele ce ne trimit la pagini
-    paginiMeniu.forEach( (link, index) => {
-      if(link.style.animation){
-        link.style.animation='';
+      // Animare butoanele ce ne trimit la pagini
+      paginiMeniu.forEach((link, index ) => {
+        if(link.style.animation)
+        { link.style.animation = '';
       }else {
-        link.style.animation=`miscareMeniu 0.5 ease forwards $(index /7 + 0.3)s
-      }
 
-    });
-  });
+
+          link.style.animation = `miscareMeniu 0.5s ease forwards ${index/7+0.3}s `  //folosim oentru animarea fiecarui element din bara de navigatie,cu o anumita intarziere intre ele
+        }
+
+       //link.style.animation=`miscareMeniu 0.5 ease forwards $(index /7 + 0.3)
+
+
+   });
+   // animatie buton de MENIU
+   butonMeniu.classList.toggle('transformare-buton-meniu')
+
+ });
 
 
 
